5a705bbdad2b3c71b955bf0990af72d3ec2c4f44
huytuan033 <hahuytuan033@gmail.com>
2024-04-01 18:04:27 +0700

    add animation for player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     
     public float speed;
+    public Animator animator;
     //get input from player
     
     private void Update()
@@ -15,6 +16,26 @@ public class Player : MonoBehaviour
 
         Vector3 direction= new Vector3(horizontal, vertical);
 
+        AnimateMovement(direction);
+
         transform.position+= direction* speed* Time.deltaTime;
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if (animator!= null)
+        {
+            if(direction.magnitude> 0)
+            {
+                animator.SetBool("isMoving", true);
+
+                animator.SetFloat("moveX", direction.x);
+                animator.SetFloat("moveY", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
     }
 }
